630bd8c87b8de76125408327c8971c79eb546fc5
AdamTieao <adamtieao@gmail.com>
2015-10-28 15:53:33 -0400

    Adding Activity about Examples

---
+++ app/src/main/java/com/team03/coursewarehub/SearchSecondExampleActivity.java
@@ -0,0 +1,13 @@
+package com.team03.coursewarehub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchSecondExampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_second_example);
+    }
+}
